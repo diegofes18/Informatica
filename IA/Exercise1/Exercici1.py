a59660b6e047ea182847c470d26698a07b924367
Diego <bermejodiego1@gmail.com>
2022-09-28 14:40:35 +0200

    ex1 ia

--- IA/Exercise1/Exercici1.py
+++ IA/Exercise1/Exercici1.py
@@ -1,26 +1,62 @@
+import csv
+
+
+def prRed(skk): print("\033[91m {}\033[00m".format(skk))
+
+
+def prGreen(skk): print("\033[92m {}\033[00m".format(skk))
+
+
+def prYellow(skk): print("\033[93m {}\033[00m".format(skk))
+
+
+def prLightPurple(skk): print("\033[94m {}\033[00m".format(skk))
+
+
+def prPurple(skk): print("\033[95m {}\033[00m".format(skk))
+
+
+def prCyan(skk): print("\033[96m {}\033[00m".format(skk))
+
+
+def prLightGray(skk): print("\033[97m {}\033[00m".format(skk))
+
+
+def prBlack(skk): print("\033[98m {}\033[00m".format(skk))
+
 class Persona:
-    def __init__(self, name:str, age: int, sex: str, heigh: int):
+    def __init__(self, name: str, age: int, sex: str, heigh: int):
         self.name = name
         self.age = age
         self.sex = sex
         self.heigh = heigh
 
     def __str__(self):
-        return self.name
+        return f"{self.name}, {self.age}, {self.sex}, {self.heigh}"
 
 
 class Estudiante(Persona):
-    def __init__(self,id:int,nota:float, name:str, age: int, sex: str, heigh: int):
-        super().__init__( name, age, sex, heigh )
+    def __init__(self, id: int, nota: float, name: str, age: int, sex: str, heigh: int):
+        super().__init__(name, age, sex, heigh)
         self.id = id
         self.nota = nota
-    
+
     def hasPassed(self):
         if self.nota > 5:
             return True
         else:
             return False
-    
+
+    def __str__(self) -> str:
+        return f"{super().__str__()}, {self.id}, {self.nota}"
+    def hasPassed(self):
+        if self.nota > 5:
+            return True
+        else:
+            return False
+
+    def toList(self):
+        return (self.name,self.age,self.sex,self.heigh,self.id,self.nota)
     def __str__(self) -> str:
         return super().__str__()
 
@@ -41,30 +77,40 @@ class Profesor(Persona):
 
         
 class Grupo:
-    def __init__(self, name:str, listest = None, prof = None):
+    def __init__(self, name:str):
         self.name = name
-        self.listest = listest
+
+
+    def set_prof(self, prof):
         self.prof = prof
 
-    def set_prof(self, prof:Profesor):
-        self.prof = prof
-
-    def set_listest(self, listaest:Estudiante):
+    def set_listest(self, listaest):
         self.listest = listaest
 
     def __str__(self) -> str:
-        return f"Nombre: {self.name}, profesor:{self.prof}"
+        return f"Nombre: {self.name}, "
+
+def toCSV(listaest):
+    outputfile = 'estudiantes.csv'
+    f = open(outputfile, 'w')
+    writer = csv.writer(f)
+    principio = ("NOM", "EDAT","SEXE", "ALTURA", "ID", "NOTA")
+    writer.writerow(principio)
+    for i in listaest:
+        writer.writerow(i.toList())
+
+    f.close()
 
 def main():
-    global nameGrup
+    global nameGrup, listaestk
     numest = int(input("Introduce el numero de estudiantes: "))
     numprof = int(input("Introduce el numero de profesores: "))
-    estudiantes = [numest]
-    profesores = [numprof]
+    estudiantes = []
+    profesores = []
 
 
     for i in range(numest):
-        print(f"Estudiante {i+1}: ")
+        prCyan(f"Estudiante {i+1}: ")
         nom = (input(f"Introduce el nombre de estudiante {i+1}: "))
         age = int(input(f"Introduce la edad del estudiante {i+1}: "))
         sex = input(f"Introduce sex del estudiante {i+1}: ")
@@ -72,8 +118,10 @@ def main():
         nota = float(input(f"Introduce la nota media del estudiante {i+1}:"))
         estudiantes.append(Estudiante(i,nota,nom,age, sex,heigh))
 
+
+
     for j in range(numprof):
-        print(f"Profesor {j+1}: ")
+        prCyan(f"Profesor {j+1}: ")
         nom = (input(f"Introduce el nombre del profesor {j+1}: "))
         age = int(input(f"Introduce la edad del profesor {j+1}: "))
         sex = input(f"Introduce sex del profesor {j+1}: ")
@@ -82,42 +130,44 @@ def main():
         capacidad = int(input(f"Introduce la capacidad media del profesor {j+1}: "))
         profesores.append(Profesor(valor,capacidad,nom,age,sex,heigh))
     
-    numgrup = int(input("Introduce el numero de grupos: "))
-    strgrupos = [numgrup]
+    numgrup = int(input("\n       Introduce el numero de grupos: "))
+    strgrupos = []
 
     for k in range(numgrup):
         nameGrup = input(f"Introduce el nombre del grupo {i+1}:")
         strgrupos.append(nameGrup)
 
-    print("\n 1 ===============Ahora vamos a asignar los profesores=================")
-    listgrup = [numgrup]
+    prGreen("\n 1 ===============Ahora vamos a asignar los profesores=================")
+    listgrup = []
     for k in range(numgrup):
         for h in profesores:
             print(f"    Profesor numero {i+1}. {h}: ")
 
+        listgrup.append(Grupo(strgrupos[k]))
         profnum = int(input(f"Cual de estos profesores es el del grupo {listgrup[k]} (introduzca el numero)"))
-        listgrup.append(Grupo(nameGrup[k],profesores[profnum-1]))
 
-    print("\n 2 ===============Ahora vamos a asignar los Estudiantes=================")
-    alumnosgrupo = [numgrup]
+        listgrup[k].set_prof(profesores[profnum-1])
 
-    for i in range(len(alumnosgrupo)):
-        alumnosgrupo[i] = int(input(f"Cuantos alumnos en el grupo {i+1}"))
-
-    print("\n 2.1 -----------Ahora vamos a asignar que grupos----------------")
+    prGreen("\n 2 ===============Ahora vamos a asignar los Estudiantes=================")
+    alumnosgrupo = []
+    prYellow("\n 2.1 -----------CUANTOS----------------")
     for i in range(numgrup):
+        x = int(input(f"Cuantos alumnos en el grupo {i+1} ?(máximo {len(estudiantes)}): "))
+        alumnosgrupo.append(x)
+
+    prYellow("\n 2.1 -----------QUIENES----------------")
+    for i in range(numgrup):
+        prPurple(f"GRUPO {strgrupos[i]}")
         for j in range(numest):
             print(f"    Estudiante {j+1}: {estudiantes[j]}")
         for k in range(alumnosgrupo[i]):
-            listaestk = [alumnosgrupo[i]]
+            listaestk = []
             j_est = int(input(f"Selecciona un estudiante de los {alumnosgrupo[i]-k} restantes: "))
             #Crear el estudiante en la lista vacia y añadirla
             listaestk.append(estudiantes[j_est-1])
         listgrup[i].set_listest(listaestk)
 
-
-#Falta csv
-
+    toCSV(estudiantes)
 
 if __name__ == '__main__':
     main()
